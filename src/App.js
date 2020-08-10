6809cd69fdb973eb4e8b3f4c26ead16f93b6eef4
sohan-bhat <64918912+sohan-bhat@users.noreply.github.com>
2020-08-10 17:07:38 -0500

    Finished all the styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
+import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 
-function App() {
-    return (
-        <div className="App">
-            <div className="container">
-                <div className="add-name">
-                    <div className="box">
+class App extends Component {
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="main">
+                    <h1 className="small-bottom-margin" id="header">List of Names</h1>
+                    <div id="new-name-form-container" className="small-bottom-margin">
                         <form>
-                            <input type="text" name="" placeholder="Add a name" />
-                            <input type="submit" name="" value="Add" />
+                        <input type="text" name="" placeholder="Add a name" />
+                        <Button 
+                            type="submit" 
+                            id="add-name-btn" 
+                            varient="contained" 
+                            style={{ 
+                                backgroundColor: "#FFE548" 
+                            }}
+                        >Add</Button>
                         </form>
                     </div>
+                    <ul id="todo-list-container" className="list-group"></ul>
                 </div>
-                    <div className="list-of-names">
-                        <ul className=""></ul>
-                    </div>
+                <Button variant="contained" color="secondary" id="reset-list-of-names-btn" startIcon={<DeleteIcon />} size="large">Reset Names</Button>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
 export default App
